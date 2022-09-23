6a4bff77c95c53f38c9c05229d0a692973af92bf
NilEis <52820950+NilEis@users.noreply.github.com>
2022-09-23 17:10:34 +0200

    added replace_char_*

--- include/clib_string.h
+++ include/clib_string.h
@@ -5,11 +5,30 @@
 #define CLIB_STRING_H
 
 /** @fn size_t clib_strcpy(char* restrict dest, const char* restrict src, size_t size)
- * @brief copies size characters of src to dest
+ * @brief copies \f$size\f$ characters of src to dest
  * @param dest The address of the destination
  * @param src The address of the src string
  * @param size The amount of characters to be copied: \f$\text{size}\leq\text{sizeof}(\text{src})\land\text{size}\leq\text{sizeof}(\text{dest})\f$
+ * @return the amount of copied characters
  */
 size_t clib_strcpy(char* restrict dest, const char* restrict src, size_t size);
+
+/** @fn int clib_replace_char(char *src, char a, char b)
+ * @brief replaces the first instance of \f$a\f$ with \f$b\f$
+ * @param src the string
+ * @param a the char to be replaced
+ * @param b the replacing char
+ * @return returns 1 if an instance was replaced, otherwise 0
+ */
+int clib_replace_char(char *src, char a, char b);
+
+/** @fn size_t clib_replace_char_all(char *src, char a, char b)
+ * @brief replaces all instances of \f$a\f$ with \f$b\f$
+ * @param src the string
+ * @param a the char to be replaced
+ * @param b the replacing char
+ * @return returns the number of replaced characters
+ */
+size_t clib_replace_char_all(char *src, char a, char b);
 
 #endif // CLIB_STRING_H
--- src/clib_string.c
+++ src/clib_string.c
@@ -10,3 +10,32 @@ size_t clib_strcpy(char *restrict dest, const char *restrict src, size_t size)
     }
     return count;
 }
+
+int clib_replace_char(char *src, char a, char b)
+{
+    while (*src != '\0')
+    {
+        if (*src == a)
+        {
+            *src = b;
+            return 1;
+        }
+        src++;
+    }
+    return 0;
+}
+
+size_t clib_replace_char_all(char *src, char a, char b)
+{
+    size_t count = 0;
+    while (*src != '\0')
+    {
+        if (*src == a)
+        {
+            *src = b;
+            count++;
+        }
+        src++;
+    }
+    return count;
+}
