3887052a9f6a27c03e6f40dc10ad126a1a8290c0
NilEis <52820950+NilEis@users.noreply.github.com>
2023-03-11 16:07:39 +0100

    added levenshtein distance

--- include/clib_string.h
+++ include/clib_string.h
@@ -4,31 +4,50 @@
 #ifndef CLIB_STRING_H
 #define CLIB_STRING_H
 
-/** @fn size_t clib_strcpy(char* restrict dest, const char* restrict src, size_t size)
+/**
+ * @fn size_t clib_string_length(const char *str)
+ * @brief computes the size of str
+ * 
+ * @param str the string
+ * @return length as size_t
+ */
+size_t clib_string_length(const char *str);
+
+/** @fn size_t clib_string_copy(char* restrict dest, const char* restrict src, size_t size)
  * @brief copies \f$size\f$ characters of src to dest
  * @param dest The address of the destination
  * @param src The address of the src string
  * @param size The amount of characters to be copied: \f$\text{size}\leq\text{sizeof}(\text{src})\land\text{size}\leq\text{sizeof}(\text{dest})\f$
  * @return the amount of copied characters
  */
-size_t clib_strcpy(char* restrict dest, const char* restrict src, size_t size);
+size_t clib_string_copy(char *restrict dest, const char *restrict src, size_t size);
 
-/** @fn int clib_replace_char(char *src, char a, char b)
+/** @fn int clib_string_replace_char(char *src, char a, char b)
  * @brief replaces the first instance of \f$a\f$ with \f$b\f$
  * @param src the string
  * @param a the char to be replaced
  * @param b the replacing char
  * @return returns 1 if an instance was replaced, otherwise 0
  */
-int clib_replace_char(char *src, char a, char b);
+int clib_string_replace_char(char *src, char a, char b);
 
-/** @fn size_t clib_replace_char_all(char *src, char a, char b)
+/** @fn size_t clib_string_replace_char_all(char *src, char a, char b)
  * @brief replaces all instances of \f$a\f$ with \f$b\f$
  * @param src the string
  * @param a the char to be replaced
  * @param b the replacing char
  * @return returns the number of replaced characters
  */
-size_t clib_replace_char_all(char *src, char a, char b);
+size_t clib_string_replace_char_all(char *src, char a, char b);
+
+/**
+ * @fn int clib_string_dist_lev(const char*a, const char*b)
+ * @brief Computes the levenshtein distance of a and b
+ *
+ * @param a string a
+ * @param b string b
+ * @returns returns the edit distance between a and b
+ */
+int clib_string_dist_lev(const char *a, const char *b);
 
 #endif // CLIB_STRING_H
--- src/clib_string.c
+++ src/clib_string.c
@@ -1,6 +1,18 @@
 #include "clib.h"
 
-size_t clib_strcpy(char *restrict dest, const char *restrict src, size_t size)
+static int __clib_string_dist_lev_rec(const char *a, const char *b, size_t as, size_t bs);
+
+size_t clib_string_length(const char *str)
+{
+    const char *end = str;
+    while (*end != '\0')
+    {
+        end++;
+    }
+    return (size_t)end - (size_t)str;
+}
+
+size_t clib_string_copy(char *restrict dest, const char *restrict src, size_t size)
 {
     size_t count = 0;
     for (size_t i = 0; i < size && src[i] != '\0'; i++)
@@ -11,7 +23,7 @@ size_t clib_strcpy(char *restrict dest, const char *restrict src, size_t size)
     return count;
 }
 
-int clib_replace_char(char *src, char a, char b)
+int clib_string_replace_char(char *src, char a, char b)
 {
     while (*src != '\0')
     {
@@ -25,7 +37,7 @@ int clib_replace_char(char *src, char a, char b)
     return 0;
 }
 
-size_t clib_replace_char_all(char *src, char a, char b)
+size_t clib_string_replace_char_all(char *src, char a, char b)
 {
     size_t count = 0;
     while (*src != '\0')
@@ -38,4 +50,32 @@ size_t clib_replace_char_all(char *src, char a, char b)
         src++;
     }
     return count;
+}
+
+int clib_string_dist_lev(const char *a, const char *b)
+{
+    return __clib_string_dist_lev_rec(a, b, clib_string_length(a), clib_string_length(b));
+}
+
+static int __clib_string_dist_lev_rec(const char *a, const char *b, size_t as, size_t bs)
+{
+    if (as == 0)
+    {
+        return (int)bs;
+    }
+    else if (bs == 0)
+    {
+        return (int)as;
+    }
+    else if (a[0] == b[0])
+    {
+        return __clib_string_dist_lev_rec(a + 1, b + 1, as - 1, bs - 1);
+    }
+    else
+    {
+        int tab = __clib_string_dist_lev_rec(a + 1, b, as - 1, bs);
+        int atb = __clib_string_dist_lev_rec(a, b + 1, as, bs - 1);
+        int tatb = __clib_string_dist_lev_rec(a + 1, b + 1, as - 1, bs - 1);
+        return 1 + (tab < atb ? (tab < tatb ? tab : tatb) : (atb < tatb ? atb : tatb));
+    }
 }
