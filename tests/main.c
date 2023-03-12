3102595a6bf92f181284c5338fa46e99c1310812
NilEis <52820950+NilEis@users.noreply.github.com>
2023-03-12 19:36:26 +0100

    added test suite and color support

--- tests/main.c
+++
@@ -1,7 +0,0 @@
-#include "test_clib_string.h"
-
-int main(int argc, char const **argv)
-{
-    test_clib_string_main();
-    return 0;
-}
--- tests/test_clib_string.c
+++ tests/test_clib_string.c
@@ -1,91 +1,95 @@
+#ifndef __BASE_FILE__
+#define __BASE_FILE__ __FILE__
+#endif
+#include "cheat.h"
 #include "test_clib_string.h"
 #include "clib.h"
+#include <assert.h>
+#include <stdio.h>
 #include <string.h>
+#if defined(_WIN32) || defined(WIN32)
+#include <windows.h>
 
-static void test_clib_string_length(void)
-{
-    const char *str = "hello world";
-    size_t len = clib_string_length(str);
-    printf("Testing %s:\n\tString: '%s', Length: %zu\n", __func__, str, len);
-    if (len != strlen(str))
+CHEAT_DECLARE(
+    
+    void init_console()
     {
-        printf("Error: clib_string_length() returned incorrect length!\n");
-    }
-    else
-    {
-        printf("PASSED\n");
-    }
-}
+        static int set = 0;
+        if(set == 1)
+        {
+            return;
+        }
+        set = 1;
+        HANDLE hOut = CreateFile("CONOUT$", GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (hOut == INVALID_HANDLE_VALUE)
+        {
+            char buf[256];
+            FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                           NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                           buf, (sizeof(buf) / sizeof(wchar_t)), NULL);
+            printf("%d - Error: %s\n", __LINE__, buf);
+            return;
+        }
 
-static void test_clib_string_copy(void)
-{
-    const char *src = "hello world";
-    char dest[20] = {0};
-    size_t size = clib_string_copy(dest, src, 5);
-    printf("Testing %s:\n\tSource: '%s', Dest: '%s', Size: %zu\n", __func__, src, dest, size);
-    if (strcmp(dest, "hello") != 0 || size != 5)
-    {
-        printf("Error: clib_string_copy() did not copy the correct number of characters!\n");
-    }
-    else
-    {
-        printf("PASSED\n");
-    }
-}
+        DWORD dwMode = 0;
+        if (GetConsoleMode(hOut, &dwMode) == 0)
+        {
+            char buf[256];
+            FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                           NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                           buf, (sizeof(buf) / sizeof(wchar_t)), NULL);
+            printf("%d - Error: %s\n", __LINE__, buf);
+            return;
+        }
 
-static void test_clib_string_replace_char(void)
-{
-    char str[] = "hello world";
-    int replaced = clib_string_replace_char(str, 'l', 'x');
-    printf("Testing %s:\n\tOriginal: '%s', Replaced: '%s', Count: %d\n", __func__, "hello world", str, replaced);
-    if (strcmp(str, "hexlo world") != 0 || replaced != 1)
-    {
-        printf("Error: clib_string_replace_char() did not replace the correct characters!\n");
-    }
-    else
-    {
-        printf("PASSED\n");
-    }
-}
+        dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        if (SetConsoleMode(hOut, dwMode) == 0)
+        {
+            char buf[256];
+            FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                           NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                           buf, (sizeof(buf) / sizeof(wchar_t)), NULL);
+            printf("%d - Error: %s\n", __LINE__, buf);
+            return;
+        }
+    })
 
-static void test_clib_string_replace_char_all(void)
-{
-    char str[] = "hello world";
-    size_t replaced = clib_string_replace_char_all(str, 'l', 'x');
-    printf("Testing %s:\n\tOriginal: '%s', Replaced: '%s', Count: %zu\n", __func__, "hello world", str, replaced);
-    if (strcmp(str, "hexxo worxd") != 0 || replaced != 3)
-    {
-        printf("Error: clib_string_replace_char_all() did not replace the correct characters!\n");
-    }
-    else
-    {
-        printf("PASSED\n");
-    }
-}
+CHEAT_SET_UP(init_console();)
+#endif
 
-static void test_clib_string_dist_lev(void)
-{
-    const char *a = "kitten";
-    const char *b = "sitting";
-    int dist = clib_string_dist_lev(a, b);
-    printf("Testing %s:\n\tString a: '%s', String b: '%s', Distance: %d\n", __func__, a, b, dist);
-    if (dist != 3)
-    {
-        printf("Error: clib_string_dist_lev() returned incorrect distance!\n");
-    }
-    else
-    {
-        printf("PASSED\n");
-    }
-}
+CHEAT_TEST(test_clib_string_length,
+           const char *const str = "hello world";
+           const size_t len = clib_string_length(str);
+           printf("- Testing %s\n", __func__);
+           cheat_assert(len == strlen(str));
+)
 
-int test_clib_string_main(void)
-{
-    printf("=======Testing string functions=======\n");
-    test_clib_string_length();
-    test_clib_string_copy();
-    test_clib_string_replace_char();
-    test_clib_string_replace_char_all();
-    test_clib_string_dist_lev();
-    return 0;
-}
+CHEAT_TEST(test_clib_string_copy,
+           const char *const src = "hello world";
+           char dest[20] = {0};
+           const size_t size = clib_string_copy(dest, src, 5);
+           printf("- Testing %s\n", __func__);
+           cheat_assert(strcmp(dest, "hello") == 0 && size == 5);
+)
+
+CHEAT_TEST(test_clib_string_replace_char,
+           char str[] = "hello world";
+           const int replaced = clib_string_replace_char(str, 'l', 'x');
+           printf("- Testing %s\n", __func__);
+           cheat_assert(strcmp(str, "hexlo world") == 0 && replaced == 1);
+)
+
+CHEAT_TEST(test_clib_string_replace_char_all,
+           char str[] = "hello world";
+           const size_t replaced = clib_string_replace_char_all(str, 'l', 'x');
+           printf("- Testing %s\n", __func__);
+           cheat_assert(strcmp(str, "hexxo worxd") == 0 && replaced == 3);
+)
+
+CHEAT_TEST(test_clib_string_dist_lev,
+           const char *const a = "kitten";
+           const char *const b = "sitting";
+           const int dist = clib_string_dist_lev(a, b);
+           printf("- Testing %s\n", __func__);
+           cheat_assert(dist == 3);
+)
