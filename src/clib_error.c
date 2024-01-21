eb5a6d34ed2f00bf2a67244658046e5cf67da29a
NilEis <52820950+NilEis@users.noreply.github.com>
2024-01-21 20:01:30 +0100

    add: beginning of sockets

--- src/clib_error.c
+++ src/clib_error.c
@@ -14,6 +14,18 @@ const char *clib_error_get_string(clib_error_code_t error_number)
         return "Could not allocate memory: calloc returned NULL";
     case CLIB_ERRNO_REALLOCATION_ERROR:
         return "Could not reallocate memory: realloc returned NULL";
+#ifdef _WIN32
+    case CLIB_ERRNO_SOCKET_WSASYSNOTREADY:
+        return "The underlying network subsystem is not ready for network communication.";
+    case CLIB_ERRNO_SOCKET_WSAVERNOTSUPPORTED:
+        return "The version of Windows Sockets support requested is not provided by this particular Windows Sockets implementation.";
+    case CLIB_ERRNO_SOCKET_WSAEINPROGRESS:
+        return "A blocking Windows Sockets 1.1 operation is in progress.";
+    case CLIB_ERRNO_SOCKET_WSAEPROCLIM:
+        return "A limit on the number of tasks supported by the Windows Sockets implementation has been reached.";
+    case CLIB_ERRNO_SOCKET_WSAEFAULT:
+        return "The lpWSAData parameter is not a valid pointer. (This error should not happen !PLEASE OPEN AN ISSUE!)";
+#endif
     case CLIB_ERRNO_STRING_INVALID_BASE:
         return "Invalid base for integer conversion";
     case CLIB_ERRNO_TREE_EMPTY:
---
+++ tests/test_clib_sockets.c
@@ -0,0 +1,74 @@
+#ifndef __BASE_FILE__
+#define __BASE_FILE__ __FILE__
+#endif
+#include "cheat.h"
+#include "clib.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdint.h>
+#if defined(_WIN32) || defined(WIN32)
+#include <windows.h>
+
+CHEAT_DECLARE(
+
+    void init_console()
+    {
+        static int set = 0;
+        if (set == 1)
+        {
+            return;
+        }
+        set = 1;
+        HANDLE hOut = CreateFile("CONOUT$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (hOut == INVALID_HANDLE_VALUE)
+        {
+            char buf[256];
+            FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                           NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                           buf, (sizeof(buf) / sizeof(wchar_t)), NULL);
+            printf("%d - Error: %s\n", __LINE__, buf);
+            return;
+        }
+
+        DWORD dwMode = 0;
+        if (GetConsoleMode(hOut, &dwMode) == 0)
+        {
+            char buf[256];
+            FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                           NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                           buf, (sizeof(buf) / sizeof(wchar_t)), NULL);
+            printf("%d - Error: %s\n", __LINE__, buf);
+            return;
+        }
+
+        dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        if (SetConsoleMode(hOut, dwMode) == 0)
+        {
+            char buf[256];
+            FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                           NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                           buf, (sizeof(buf) / sizeof(wchar_t)), NULL);
+            printf("%d - Error: %s\n", __LINE__, buf);
+            return;
+        }
+    })
+
+#else
+
+CHEAT_DECLARE(
+
+    void init_console() {
+        return;
+    })
+
+#endif
+
+CHEAT_TEST(
+    clib_sockets_init,
+    printf("- Testing %s\n", __func__);
+    clib_error_code_t ret = clib_sockets_init();
+    if (ret != CLIB_ERRNO_NO_ERROR)
+    {
+        printf("Error: %s\n", clib_error_get_string(ret));
+    };
+    cheat_assert(ret == CLIB_ERRNO_NO_ERROR);)
