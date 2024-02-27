dc82817f63db1f7fbaf160eecf3fef77d19c0ec3
NilEis <52820950+NilEis@users.noreply.github.com>
2024-02-27 21:23:18 +0100

    fix: cmake

--- src/clib_file.c
+++ src/clib_file.c
@@ -1,7 +1,7 @@
 #include "clib_file.h"
 #include <stdlib.h>
 #ifdef CLIB_INCLUDE_FILE
-#ifdef CLIB_FILE_SELECTOR
+#if defined(CLIB_FILE_SELECTOR) && !CLIB_ANSI
 #include "nfd.h"
 #endif /* CLIB_FILE_SELECTOR */
 
@@ -9,9 +9,9 @@ size_t clib_file_get_size(FILE *file)
 {
     size_t res;
     size_t p = (size_t)ftell(file);
-    fseek(file, 0, SEEK_END);
+    (void)fseek(file, 0, SEEK_END);
     res = (size_t)ftell(file);
-    fseek(file, p, SEEK_SET);
+    (void)fseek(file, (long)p, SEEK_SET);
     return (size_t)res;
 }
 
@@ -20,7 +20,7 @@ const char *clib_file_load(const char *path)
     FILE *f = fopen(path, "r");
     size_t size = clib_file_get_size(f);
     char *res = calloc(size + 1, sizeof(char));
-    (void)fread(res, sizeof(char), size, f);
+    (void)fread(res, sizeof(char), (unsigned)size, f);
     fclose(f);
     return (const char *)res;
 }
