3344beecce5ce8757a88b2f4666244c6179adf39
NilEis <52820950+NilEis@users.noreply.github.com>
2024-03-06 19:18:37 +0100

    add: clang tidy and format

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -3,3 +3,11 @@ option(TCC "Generate tcc makefile in build" OFF)
 option(BUILD_WITH_DOCS "Generate Docs" OFF)
 
 option(BUILD_TESTS "Build test exe" OFF)
+
+option(SUPPORT_CLANGD "Support clangd" ON)
+if(SUPPORT_CLANGD)
+  set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+  file(TOUCH ${CMAKE_CURRENT_BINARY_DIR}/compile_commands.json)
+  file(CREATE_LINK ${CMAKE_CURRENT_BINARY_DIR}/compile_commands.json
+       ${CMAKE_CURRENT_SOURCE_DIR}/compile_commands.json COPY_ON_ERROR)
+endif()
