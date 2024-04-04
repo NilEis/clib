930b6e89a04ca8f43229ee0086a07a161bbb2d6f
NilEis <52820950+NilEis@users.noreply.github.com>
2024-04-04 19:07:53 +0200

    ref(cpuid): remove namened registers and added normal bitfields

--- src/cpuid/clib_cpuid_checks.c
+++ src/cpuid/clib_cpuid_checks.c
@@ -1,40 +1,40 @@
 #include "clib_cpuid.h"
 #include <stddef.h>
 
-#define ADD_CHECK(leaf, subleaf, reg, name)                                   \
+#define ADD_CHECK(leaf, subleaf, name)                                   \
     int clib_cpuid_supports_##name (void)                                     \
     {                                                                         \
         clib_cpuid_leaf_##leaf##_subleaf_##subleaf##_t *res                   \
             = clib_cpuid_get (leaf, subleaf);                                 \
-        return res->reg.field.name;                                           \
+        return res->name;                                           \
     }
 
-ADD_CHECK (1, 0, edx, cmov)
-ADD_CHECK (1, 0, edx, mmx)
-ADD_CHECK (1, 0, ecx, popcnt)
-ADD_CHECK (1, 0, edx, sse)
-ADD_CHECK (1, 0, edx, sse2)
-ADD_CHECK (1, 0, ecx, sse3)
-ADD_CHECK (1, 0, ecx, ssse3)
-ADD_CHECK (1, 0, ecx, sse4_1)
-ADD_CHECK (1, 0, ecx, sse4_2)
-ADD_CHECK (1, 0, ecx, avx)
-ADD_CHECK (7, 0, ebx, avx2)
-ADD_CHECK (0x80000001, 0, ecx, sse4a)
-ADD_CHECK (1, 0, ecx, fma)
-ADD_CHECK (0x80000001, 0, ecx, fma4)
-ADD_CHECK (0x80000001, 0, ecx, xop)
-ADD_CHECK (7, 0, ebx, avx512f)
-ADD_CHECK (7, 0, ebx, bmi1)
-ADD_CHECK (7, 0, ebx, bmi2)
-ADD_CHECK (1, 0, ecx, aes)
-ADD_CHECK (7, 0, ebx, avx512vl)
-ADD_CHECK (7, 0, ebx, avx512bw)
-ADD_CHECK (7, 0, ebx, avx512dq)
-ADD_CHECK (7, 0, ebx, avx512cd)
-ADD_CHECK (7, 0, ebx, avx512er)
-ADD_CHECK (7, 0, ebx, avx512pf)
-ADD_CHECK (7, 0, ecx, avx512vbmi)
-ADD_CHECK (7, 0, ebx, avx512ifma)
-ADD_CHECK (7, 0, ecx, gfni)
-ADD_CHECK (7, 0, ecx, vpclmulqdq)
+ADD_CHECK (1, 0, cmov)
+ADD_CHECK (1, 0, mmx)
+ADD_CHECK (1, 0, popcnt)
+ADD_CHECK (1, 0, sse)
+ADD_CHECK (1, 0, sse2)
+ADD_CHECK (1, 0, sse3)
+ADD_CHECK (1, 0, ssse3)
+ADD_CHECK (1, 0, sse4_1)
+ADD_CHECK (1, 0, sse4_2)
+ADD_CHECK (1, 0, avx)
+ADD_CHECK (7, 0, avx2)
+ADD_CHECK (0x80000001, 0, sse4a)
+ADD_CHECK (1, 0, fma)
+ADD_CHECK (0x80000001, 0, fma4)
+ADD_CHECK (0x80000001, 0, xop)
+ADD_CHECK (7, 0, avx512f)
+ADD_CHECK (7, 0, bmi1)
+ADD_CHECK (7, 0, bmi2)
+ADD_CHECK (1, 0, aes)
+ADD_CHECK (7, 0, avx512vl)
+ADD_CHECK (7, 0, avx512bw)
+ADD_CHECK (7, 0, avx512dq)
+ADD_CHECK (7, 0, avx512cd)
+ADD_CHECK (7, 0, avx512er)
+ADD_CHECK (7, 0, avx512pf)
+ADD_CHECK (7, 0, avx512vbmi)
+ADD_CHECK (7, 0, avx512ifma)
+ADD_CHECK (7, 0, gfni)
+ADD_CHECK (7, 0, vpclmulqdq)
